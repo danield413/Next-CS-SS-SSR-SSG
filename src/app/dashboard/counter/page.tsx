8f6cdd4fc227a85964746366483f4cc747920a77
Daniel Dìaz <danieldiazc413@gmail.com>
2024-07-10 09:45:12 -0500

    Server side (SS) + Client side (CS)

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+
+import { useState } from "react";
+import { Metadata } from "next";
+import { CartCounter } from "@/app/shopping-cart/components";
+
+export const metadata: Metadata = {
+  title: 'Shopping Cart',
+  description: 'Un simple contador'
+}
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      
+      <CartCounter value={ 20 }/>
+    </div>
+  );
+}
